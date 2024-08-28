352db0bb0e8c33c4f14cb9f087c84be9408a0fb4
oneachina <ruiruixi@hotmail.com>
2024-08-28 17:16:35 +0800

    food

--- src/main/java/cn/onea/MITEItemGroups.java
+++ src/main/java/cn/onea/MITEItemGroups.java
@@ -9,7 +9,7 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 import static cn.onea.MITEItems.SALAD;
-//import static cn.onea.MITEItems.WOODEN_CUDGEL;
+import static cn.onea.MITEItems.blueberries;
 
 public final class MITEItemGroups {
     public static final ItemGroup MiteFood = Registry.register(Registries.ITEM_GROUP, Identifier.of("mite", "mitefood"), FabricItemGroup.builder()
@@ -17,15 +17,9 @@ public final class MITEItemGroups {
             .displayName(Text.translatable("MiteFood"))
             .entries((context, entries) -> {
                 entries.add(SALAD);
+                entries.add(blueberries);
             })
             .build());
-    //public static final ItemGroup MiteCombot = Registry.register(Registries.ITEM_GROUP, Identifier.of("mite", "mitecombot"), FabricItemGroup.builder()
-    //        .icon(() -> new ItemStack(WOODEN_CUDGEL))
-    //       .displayName(Text.translatable("MiteComBot"))
-    //       .entries((context, entries) -> {
-    //          entries.add(WOODEN_CUDGEL);
-    //      })
-    //      .build());
     public static void initialize() {
     }
 }
--- src/main/java/cn/onea/MITEItems.java
+++ src/main/java/cn/onea/MITEItems.java
@@ -10,17 +10,17 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public final class MITEItems {
-    public static final Wooden_Cudgel INSTANCE = new Wooden_Cudgel();
     private MITEItems() {}
     //FOODComponent
-    public static final FoodComponent SALAD_FOOD = new FoodComponent.Builder()
+    public static final FoodComponent Food = new FoodComponent.Builder()
             .snack()
             .nutrition(2)
             .saturationModifier(2.0F)
             .build();
     //Item
-    //public static final Item WOODEN_CUDGEL = register(new SwordItem(INSTANCE, new Item.Settings()), "Wooden_Cudgel");
-    public static final Item SALAD = register(new Item(new Item.Settings().maxCount(4).food(SALAD_FOOD)), "salad");
+    public static final Item SALAD = register(new Item(new Item.Settings().maxCount(4).food(Food)), "salad");
+    public static final Item blueberries = register(new Item(new Item.Settings().maxCount(4).food(Food)), "blueberries");
+
     public static Item register(Item item, String id) {
         Identifier itemID = Identifier.of(MITE.MOD_ID, id);
         Item registeredItem = Registry.register(Registries.ITEM, itemID, item);
