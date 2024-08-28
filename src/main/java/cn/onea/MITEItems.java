5464f83b1a40faf285c4160d27bedcf16b6161c4
oneachina <ruiruixi@hotmail.com>
2024-08-28 16:43:53 +0800

    重写item register方法

--- src/main/java/cn/onea/MITEItems.java
+++ src/main/java/cn/onea/MITEItems.java
@@ -1,19 +1,30 @@
 package cn.onea;
 
-import cn.onea.Item.Salad;
+import cn.onea.Item.Food.Salad;
+import cn.onea.Item.Combot.Wooden_Cudgel;
+import net.minecraft.component.type.FoodComponent;
 import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public final class MITEItems {
+    public static final Wooden_Cudgel INSTANCE = new Wooden_Cudgel();
     private MITEItems() {}
-    // 新物品的实例
-    public static final Salad SALAD = register("salad", new Salad(new Item.Settings().maxCount(4)));
-
-    public static <T extends Item> T register(String path, T item) {
-        // 对于 1.21 之前的版本，请将 ''Identifier.of'' 替换为 ''new Identifier''
-        return Registry.register(Registries.ITEM, Identifier.of("mite", path), item);
+    //FOODComponent
+    public static final FoodComponent SALAD_FOOD = new FoodComponent.Builder()
+            .snack()
+            .nutrition(2)
+            .saturationModifier(2.0F)
+            .build();
+    //Item
+    public static final Item WOODEN_CUDGEL = register(new SwordItem(INSTANCE, new Item.Settings()), "Wooden_Cudgel");
+    public static final Item SALAD = register(new Item(new Item.Settings().maxCount(4).food(SALAD_FOOD)), "salad");
+    public static Item register(Item item, String id) {
+        Identifier itemID = Identifier.of(MITE.MOD_ID, id);
+        Item registeredItem = Registry.register(Registries.ITEM, itemID, item);
+        return registeredItem;
     }
 
     public static void initialize() {
